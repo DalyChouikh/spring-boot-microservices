35f490f67d63dc75ca793cf9a4f90f0e9c8c9e21
DalyChouikh <chouikhdaly215@gmail.com>
2023-02-05 23:53:27 +0100

    Added discovery-service & Enabled Eureka Server

--- inventory-service/src/main/java/dev/daly/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/dev/daly/inventoryservice/InventoryServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
-
 @SpringBootApplication
 public class InventoryServiceApplication {
 
