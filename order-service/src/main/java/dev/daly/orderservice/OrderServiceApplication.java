70920a9654dd4dc27495b72da0d2cfe0b45a8e8d
DalyChouikh <chouikhdaly215@gmail.com>
2023-02-16 16:38:03 +0100

    Configured Event driven architecture using Kafka

--- order-service/src/main/java/dev/daly/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/dev/daly/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package dev.daly.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class OrderServiceApplication {
